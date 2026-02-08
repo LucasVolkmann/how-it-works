6e33cb9e89ad13c697eec3c020189a52dd15c524
Lucas Volkmann <lucas.volkmann2001@gmail.com>
2026-02-08 17:27:57 -0300

    feat: Add 15min option in jwt expiresIn

--- src/types/environment/jwt-env.interface.ts
+++ src/types/environment/jwt-env.interface.ts
@@ -1,4 +1,4 @@
-export type ExpiresInValueType = "1d" | "1h";
+export type ExpiresInValueType = "1d" | "1h" | "15min";
 
 export interface IJWTEnv {
     expiresIn: ExpiresInValueType;
--- src/types/guards/is-expires-in.guard.ts
+++ src/types/guards/is-expires-in.guard.ts
@@ -4,7 +4,8 @@ import type { ExpiresInValueType } from "../environment/jwt-env.interface.js";
 export function isExpiresInValue(value: string): value is ExpiresInValueType {
     const possibleValues = [
         "1d",
-        "1h"
+        "1h",
+        "15min"
     ]
     return possibleValues.includes(value);
 }
