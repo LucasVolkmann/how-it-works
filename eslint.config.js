e0356077620ae8adfdc7c4425d10c17ca6552a40
Lucas Volkmann <lucas.volkmann2001@gmail.com>
2026-02-08 18:12:43 -0300

    chore: Add eslint and prettier

---
+++ eslint.config.js
@@ -0,0 +1,62 @@
+import { defineConfig } from 'eslint/config';
+import globals from 'globals';
+import eslintPluginPrettier from 'eslint-plugin-prettier';
+import tseslint from '@typescript-eslint/eslint-plugin';
+import tsParser from '@typescript-eslint/parser';
+
+export default defineConfig([
+  {
+    ignores: ['node_modules/**', 'dist/**', 'build/**', 'coverage/**'],
+
+    languageOptions: {
+      ecmaVersion: 2022,
+      sourceType: 'module',
+      globals: {
+        ...globals.node,
+        myCustomGlobal: 'readonly',
+      },
+    },
+
+    plugins: {
+      prettier: eslintPluginPrettier,
+    },
+
+    rules: {
+      'prettier/prettier': 'error',
+
+      'no-console': 'warn',
+      eqeqeq: ['error', 'always'],
+      'no-var': 'error',
+      'prefer-const': 'error',
+      'no-debugger': 'error',
+    },
+  },
+
+  {
+    files: ['**/*.ts'],
+    ignores: ['node_modules/**', 'dist/**', 'build/**', 'coverage/**'],
+    languageOptions: {
+      parser: tsParser,
+      parserOptions: {
+        project: './tsconfig.json',
+        sourceType: 'module',
+      },
+    },
+    plugins: {
+      '@typescript-eslint': tseslint,
+    },
+    rules: {
+      'no-unused-vars': 'off',
+      'no-undef': 'off',
+
+      '@typescript-eslint/no-unused-vars': [
+        'warn',
+        { argsIgnorePattern: '^_', varsIgnorePattern: '^_' },
+      ],
+
+      '@typescript-eslint/no-explicit-any': 'off',
+      '@typescript-eslint/explicit-function-return-type': 'off',
+      '@typescript-eslint/no-non-null-assertion': 'off',
+    },
+  },
+]);
