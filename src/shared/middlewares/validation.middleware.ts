018e823c35c4579ad4f4fc26eda2e4a8e9211151
Lucas Volkmann <lucas.volkmann2001@gmail.com>
2026-02-08 16:48:19 -0300

    refactor: Format and type validation middleware

--- src/shared/middlewares/validation.middleware.ts
+++ src/shared/middlewares/validation.middleware.ts
@@ -2,16 +2,18 @@ import type { NextFunction, Request, Response } from "express";
 import type { ZodType } from "zod";
 import { ValidationError } from "../errors/validation.error.js";
 
-export function validateBody(schema: ZodType) {
+export function validateBody<T>(schema: ZodType<T>) {
   return (req: Request, res: Response, next: NextFunction) => {
     const result = schema.safeParse(req.body);
 
     if (!result.success) {
-      const errors = result.error.issues.map(err => `${err.path}: ${err.message}`);
+      const errors = result.error.issues.map(
+        err => `${err.path}: ${err.message}`
+      );
       throw new ValidationError(errors);
     }
 
-    req.body = result.data;
+    req.body = result.data as T;
     return next();
   };
 }
