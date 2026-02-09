19eb5f0c08d10bf1d571f66803a62de697e4b662
Lucas Volkmann <lucas.volkmann2001@gmail.com>
2026-02-09 18:30:43 -0300

    chore: Turn off console warnings into esling configs

--- eslint.config.js
+++ eslint.config.js
@@ -24,7 +24,7 @@ export default defineConfig([
     rules: {
       'prettier/prettier': 'error',
 
-      'no-console': 'warn',
+      'no-console': 'off',
       eqeqeq: ['error', 'always'],
       'no-var': 'error',
       'prefer-const': 'error',
