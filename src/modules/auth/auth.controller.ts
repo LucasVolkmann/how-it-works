1365556b4ae5be91d90024722b621c71d4e5c265
Lucas Volkmann <lucas.volkmann2001@gmail.com>
2026-02-26 14:36:27 -0300

    refactor: Remove literal status codes from auth controller. Uses http-status-codes lib.

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -2,6 +2,7 @@ import type { Request, Response } from 'express';
 import { AppDataSource } from '../../config/data-source.config.js';
 import { User } from '../../domain/entities/user.entity.js';
 import { AuthService } from './auth.service.js';
+import { StatusCodes } from 'http-status-codes';
 
 const userRepository = AppDataSource.getRepository(User);
 const authService = new AuthService(userRepository);
@@ -9,11 +10,11 @@ const authService = new AuthService(userRepository);
 export class AuthController {
   async register(req: Request, res: Response) {
     const result = await authService.register(req.body);
-    return res.status(201).json(result);
+    return res.status(StatusCodes.CREATED).json(result);
   }
 
   async login(req: Request, res: Response) {
     const result = await authService.login(req.body);
-    return res.status(200).json(result);
+    return res.json(result);
   }
 }
