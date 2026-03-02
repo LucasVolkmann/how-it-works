f47b835ebc10351491f53f3abf63eb71aaef4235
Lucas Volkmann <lucas.volkmann2001@gmail.com>
2026-03-02 14:03:51 -0300

    refactor: Use status codes lib to remove magic numbers from not-found middleware

--- src/shared/middlewares/not-found.middleware.ts
+++ src/shared/middlewares/not-found.middleware.ts
@@ -1,6 +1,7 @@
 import type { RequestHandler } from 'express';
 import createHttpError from 'http-errors';
+import { StatusCodes } from 'http-status-codes';
 
 export const routeNotFound: RequestHandler = async (req, res, next) => {
-  next(createHttpError(404, 'Rota não encontrada'));
+  next(createHttpError(StatusCodes.NOT_FOUND, 'Rota não encontrada'));
 };
