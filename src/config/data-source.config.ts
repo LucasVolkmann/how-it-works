54dc48eff53d3a7cc09b866f44f0588868bec2b3
Lucas Volkmann <lucas.volkmann2001@gmail.com>
2026-03-06 16:48:11 -0300

    test: Create data source for tests, using memory sqlite

--- src/config/data-source.config.ts
+++ src/config/data-source.config.ts
@@ -5,15 +5,30 @@ import { SnakeNamingStrategy } from './snake-naming-strategy.config.js';
 import { User } from '../domain/entities/user.entity.js';
 import { Post } from '../domain/entities/post.entity.js';
 
-export const AppDataSource = new DataSource({
-  type: 'postgres',
-  host: env.db.host,
-  port: env.db.port,
-  username: env.db.user,
-  password: env.db.password,
-  database: env.db.database,
-  namingStrategy: new SnakeNamingStrategy(),
-  synchronize: env.nodeEnv === 'development',
-  logging: true,
-  entities: [Post, User],
-});
+export const AppDataSource =
+  env.nodeEnv === 'test' ? createTestDataSource() : createProductionDataSource();
+
+function createProductionDataSource() {
+  return new DataSource({
+    type: 'postgres',
+    host: env.db.host,
+    port: env.db.port,
+    username: env.db.user,
+    password: env.db.password,
+    database: env.db.database,
+    namingStrategy: new SnakeNamingStrategy(),
+    synchronize: env.nodeEnv === 'development',
+    logging: true,
+    entities: [Post, User],
+  });
+}
+
+function createTestDataSource() {
+  return new DataSource({
+    type: 'sqlite',
+    database: ':memory:',
+    synchronize: true,
+    logging: false,
+    entities: [User, Post],
+  });
+}
