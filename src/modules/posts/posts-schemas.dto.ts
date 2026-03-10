339db68d9d09933cc3016744bbbb090646df21a9
Lucas Volkmann <lucas.volkmann2001@gmail.com>
2026-03-10 17:51:57 -0300

    feat: Add custom validation error messagens from zod schemas

--- src/modules/posts/posts-schemas.dto.ts
+++ src/modules/posts/posts-schemas.dto.ts
@@ -1,25 +1,41 @@
 import { z } from 'zod';
 
 export const createPostSchema = z.object({
-  title: z.string().min(3),
-  content: z.string().min(10),
-  published: z.boolean().optional(),
+  title: z
+    .string('O título é obrigatório e deve ser um texto.')
+    .min(3, 'O título deve ter pelo menos 3 caracteres.'),
+  content: z
+    .string('O conteúdo é obrigatório e deve ser um texto.')
+    .min(10, 'O conteúdo deve ter pelo menos 10 caracteres.'),
+  published: z
+    .boolean('O campo "publicado" deve ser verdadeiro ou falso.')
+    .optional(),
 });
 export type CreatePostDTO = z.infer<typeof createPostSchema>;
 
 export const updatePostSchema = z.object({
-  title: z.string().min(3).optional(),
-  content: z.string().min(10).optional(),
-  published: z.boolean().optional(),
+  title: z
+    .string('O título deve ser um texto.')
+    .min(3, 'O título deve ter pelo menos 3 caracteres.')
+    .optional(),
+  content: z
+    .string('O conteúdo deve ser um texto.')
+    .min(10, 'O conteúdo deve ter pelo menos 10 caracteres.')
+    .optional(),
+  published: z
+    .boolean('O campo "publicado" deve ser verdadeiro ou falso.')
+    .optional(),
 });
 export type UpdatePostDTO = z.infer<typeof updatePostSchema>;
 
 export const getBySlugParamsSchema = z.object({
-  slug: z.string().min(3),
+  slug: z
+    .string('O slug é obrigatório e deve ser um texto.')
+    .min(3, 'O slug deve ter pelo menos 3 caracteres.'),
 });
 export type GetBySlugParamsDTO = z.infer<typeof getBySlugParamsSchema>;
 
 export const paramsWithIdSchema = z.object({
-  id: z.guid(),
+  id: z.guid('O ID informado não é um UUID válido.'),
 });
 export type ParamsWithIdDTO = z.infer<typeof paramsWithIdSchema>;
