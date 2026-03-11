f39c2c81d98b3ca5bd7e60ed2bc8d1c57339fb79
Lucas Volkmann <lucas.volkmann2001@gmail.com>
2026-03-11 15:06:43 -0300

    feat: Add custom validation messages for users payloads

--- src/modules/users/users-schemas.dto.ts
+++ src/modules/users/users-schemas.dto.ts
@@ -1,6 +1,9 @@
 import { z } from 'zod';
 
 export const updateUserSchema = z.object({
-  name: z.string().min(3).optional(),
+  name: z
+    .string('O nome deve ser um texto.')
+    .min(3, 'O nome deve ter pelo menos 3 caracteres.')
+    .optional(),
 });
 export type UpdateUserDTO = z.infer<typeof updateUserSchema>;
